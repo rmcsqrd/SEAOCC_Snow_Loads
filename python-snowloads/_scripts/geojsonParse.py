536ff162e00b73cf4a46fef0a294cda7c8540fee
riomcmahon <48159564+rmcsqrd@users.noreply.github.com>
2019-07-07 22:29:49 -0700

    files working, now need to show up on map

--- python-snowloads/_scripts/geojsonParse.py
+++ python-snowloads/_scripts/geojsonParse.py
@@ -15,38 +15,7 @@ with open(dataGetDirectory+"Counties_April_20.geojson","r") as file:
     data = json.load(file)
 
 ## Parse JSON ##
-
-# RAW DATA ORGANIZATION
-# this website is also really helpful http://jsonviewer.stack.hu/
-
-# features (list)
-#     geometry (dict)
-#         --> type
-#             --> Geometry Collection
-#         --> geometries
-#             --> Point
-#             --> Polygon/Multipolygon ***these are the coordinates we want
-#     properties (dict)
-#         --> Name
-#         --> HAWN_PI
-#         --> OTHER
-#         --> AMERI_ES
-#         --> description ***this is the description we want
-#         --> HISPANIC
-#         --> snippet
-#         --> WHITE
-#         --> POP00_SQMI
-#         --> MALES
-#         --> BLACK
-#         --> FEMALES
-#         --> tessellate
-#         --> visibility
-#         --> POP2000
-#         --> MULT_RACE
-#         --> ASIAN
-#         --> SQMI
-#     type (str)
-#         --> Feature (this is just a descriptor, doesn't contain any information)
+# this website is really helpful to visualize data structure http://jsonviewer.stack.hu/
 
 features = data['features']  # list of dictionaries
 mapDict = {}  # final output dict to be written into leaflet format
@@ -57,11 +26,30 @@ for cnt, item in enumerate(features):  # step through all values within file, no
         if item['properties']['description'] is not None:  #  get a title for the dictionary - we only want values that have a description
             description = item['properties']['description']
             title = re.search("(?<=<p>)(.*?)(?=<\/p>)", description).group(0)  # regex code hacked together from stack overflow
+            title = title.replace(' ', '')
 
             if 'geometries' in item['geometry']:  # some containers are empty and return error
                 if item['geometry']['geometries'][0]['type'] is not 'Point':  # coords have arbitrary (?) point data
-                    coords = (item['geometry']['geometries'][1]['coordinates'])  # coords are nested a few times
+                    coords = item['geometry']['geometries'][1]['coordinates'][0]  # coords are nested a few times
+                    for subitem in coords:
+                        subitem.reverse() # very confusing but coordinates are flipper apparently
+
         mapDict[title] = [description, coords]
 
-for k, v in mapDict.items():
-    print(k, v)
+## Write to file ##
+
+# this is where I got really lazy - need to update this in the future when we have more time
+file = open(dataWriteDirectory+"leaflet_input.txt","w")
+for k,v in sorted(mapDict.items()):
+    print(k)
+    file.write('var '+ k + ' = L.polygon([\n')
+    for cnt, item in enumerate(v[1]):  # really unelegant method for not adding a comma to end
+        if (cnt+1) != len(v[1]):
+            file.write('%s' % item +',\n')
+        else:
+            file.write('%s' % item + '\n')
+    file.write(']).addTo(mymap);')
+    file.write('\n')
+    file.write(k + '.bindPopup("' + v[0] + '");')
+    file.write('\n\n')
+file.close()
